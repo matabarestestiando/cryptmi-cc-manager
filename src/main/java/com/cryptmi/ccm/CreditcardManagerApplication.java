1e603320e47a97f2238bdc5e2b18eac6ad333f77
Matabares <matabares@gmail.com>
2022-12-23 14:43:32 -0500

    initial commit

---
+++ src/main/java/com/cryptmi/ccm/CreditcardManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cryptmi.ccm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcardManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptmi/ccm/CreditcardManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptmi.ccm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditcardManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
